edd5f887f76c13c52b78e7e60865cca01e9cd637
Chen Zou <zouch@oregonstate.edu>
2020-05-07 21:32:43 -0700

    worked

--- OAuth2.0/main.py
+++ OAuth2.0/main.py
@@ -1,46 +1,61 @@
 import json
-import flask
+from flask import Flask, session, render_template, request, redirect, url_for
 import requests
+import secrets
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 CLIENT_ID = '793207193577-b0m59mshitdvarg41a6q8e3ofl2sh14m.apps.googleusercontent.com'
 CLIENT_SECRET = 'iOr-aGJifs75GOvIQkCVx1aa'
 SCOPE = 'profile email openid'
 REQUEST_URL = "https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses"
-REDIRECT_URI = 'http://localhost:8080/user_info'
+REDIRECT_URI = 'http://localhost:8080/auth'
 
-
-@app.route('/')
-def index():
-  if 'credentials' not in flask.session:
-    return flask.redirect(flask.url_for('user_info'))
-  credentials = json.loads(flask.session['credentials'])
-  if credentials['expires_in'] <= 0:
-    return flask.redirect(flask.url_for('user_info'))
-  else:
-    headers = {'Authorization': 'Bearer {}'.format(credentials['access_token'])}
-    r = requests.get(REQUEST_URL, headers=headers)
-    return r.text
-
-
-@app.route('/user_info')
-def user_info():
-  if 'code' not in flask.request.args:
-    auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
-                '&client_id={}&redirect_uri={}&scope={}').format(CLIENT_ID, REDIRECT_URI, SCOPE)
-    return flask.redirect(auth_uri)
-  else:
-    auth_code = flask.request.args.get('code')
+# post auth code to server to get credentials including access token
+def get_credentials(auth_code):
     data = {'code': auth_code,
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
             'redirect_uri': REDIRECT_URI,
             'grant_type': 'authorization_code'}
-    r = requests.post('https://oauth2.googleapis.com/token', data=data)
-    flask.session['credentials'] = r.text
-    return flask.redirect(flask.url_for('index'))
+    response = requests.post('https://oauth2.googleapis.com/token', data=data)
+    return response.json()
+
+
+@app.route('/')
+def index():
+        return render_template('welcome.html', error_message=request.args.get('error_message',''))
+
+
+@app.route('/auth')
+def auth():
+    if 'code' not in request.args or 'state' not in request.args:
+        session['state'] = secrets.token_urlsafe(16)
+        print('generated state is: ', session['state'])
+        auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
+                '&client_id={}&redirect_uri={}&scope={}&state={}').format(CLIENT_ID, REDIRECT_URI, SCOPE, session['state'])
+        return redirect(auth_uri)
+    if request.args.get('state') != session['state']:
+        print("returned state is: ", request.args.get('state'))
+        return redirect(url_for('index',error_message='State value mismatch, please try again'))
+    else:
+        auth_code = request.args.get('code')
+        credentials = get_credentials(auth_code)
+        if 'access_token' not in credentials:
+            return redirect(url_for('index', error_message='Authorization failed, please try again'))
+        if credentials['expires_in'] <= 0:
+            return redirect(url_for('index', error_message='Token expired, please try again'))
+        session['credentials'] = credentials
+        return redirect(url_for('user_info'))
+
+@app.route('/user_info')
+def user_info():
+    credentials = session['credentials']
+    headers = {'Authorization': 'Bearer {}'.format(credentials['access_token'])}
+    data = requests.get(REQUEST_URL, headers=headers)
+    names = data.json()['names'][0]
+    return render_template('user_info.html',names = names, state=session['state'])
 
 
 if __name__ == '__main__':
